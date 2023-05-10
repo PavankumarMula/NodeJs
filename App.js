bd93f6fc10b2b170223d0b3942278f732c891518
PavankumarMula <pavanpappy7@gmail.com>
2023-05-11 01:23:59 +0530

    Server Is created on the Port 4000

---
+++ App.js
@@ -0,0 +1,8 @@
+const http=require('http');
+const server=http.createServer((req,res)=>{
+   //do domething with request and response
+});
+server.listen(4000,()=>{
+    console.log("Pavan Kumar Mula");
+})
+
