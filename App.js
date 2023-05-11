95bce2684bcf6934b313fb97f75965692c5dd7da
PavankumarMula <pavanpappy7@gmail.com>
2023-05-12 00:27:44 +0530

    sent the response to Client

--- App.js
+++ App.js
@@ -1,8 +1,22 @@
+//importing http module
+const { write } = require('fs');
 const http=require('http');
+//creating server 
 const server=http.createServer((req,res)=>{
-   //do domething with request and response
+   const url=req.url;
+   if(url==="/"){
+    res.write("Hello From Node Js");
+   }
+   else if(url==="/home"){
+    res.write("Welcome Home");
+   }
+   else if(url==="/home"){
+    res.write("Welcome Home");
+   }
+   else if(url==="/About"){
+        res.write("Welcome to About Page");
+   }
+   res.end();
 });
-server.listen(4000,()=>{
-    console.log("Pavan Kumar Mula");
-})
+server.listen(5000);
 
